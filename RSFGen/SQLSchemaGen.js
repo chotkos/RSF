fe83a39f66701e918e7ff50a7cfd70e673ba838a
chotkos <chotkos@gmail.com>
2018-11-12 12:24:20 +0100

    Update SQLSchemaGen.js

--- RSFGen/SQLSchemaGen.js
+++ RSFGen/SQLSchemaGen.js
@@ -1,6 +1,30 @@
-var getSQLForDefinition= function(definition)  {
-    
+
+var getType = function(column,columnName){
+    switch(column.type){
+        case "integer":
+            if(columnName == "Id"){
+                return "INT32 PRIMARY KEY"
+            } else{
+                return "INT32";
+            }
+            break;
+        case "string":
+            if(column.format === "date-time"){
+                return " DATETIME";
+            } else {
+                return " VARCHAR(100)";
+            }
+            break;
+        case "boolean":
+            return " BIT"
+            break;  
+        default:
+            var refTable = column["$ref"].split('definitions/')[0];
+            return "Id INT FOREIGN KEY REFERENCES "+refTable+"refTable(Id)";
+            break
+    }
 };
+ 
 
 var generateSQLTables = function(definitions){
     var result = "";
@@ -12,10 +36,11 @@ var generateSQLTables = function(definitions){
                 .forEach(columnName => {
                     var col =  columns[columnName]
                     console.log(col);
+                    var type = getType(col,columnName); 
+                    tableResult+= "ALTER TABLE "+ entityName + " ADD " + columnName + type +" "+ key"; ";
                 });
-            
-            
             
             result+= tableResult
         });
+    console.log(result);
 };
