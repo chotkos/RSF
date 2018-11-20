02973aa633862299066e9c4a03e982f42952d3d7
chotkos <chotkos@gmail.com>
2018-11-20 15:18:32 +0100

    Update DTOGen.js

--- RSFGen/DTOGen.js
+++ RSFGen/DTOGen.js
@@ -1,13 +1,50 @@
 var generateDTO = function(){
 
+	var properties = "\n";
+    var entityName = $('#model-select').val();
+	var propertiesObj = document.data.definitions[entityName].properties;
+
+	Object.keys(propertiesObj)
+		.forEach(columnName => {
+			if(columnName != "Id")
+			{
+				var column = propertiesObj[columnName];
+				console.log(columnName);
+				switch(column.type){
+					case "integer":
+						properties += "\t\tpublic int "+columnName+" { get; set; }\n";
+						break;
+					case "string":
+						if(column.format === "date-time"){
+							properties += "\t\tpublic DateTime "+columnName+" { get; set; }\n";
+						} else {
+							properties += "\t\tpublic string "+columnName+" { get; set; }\n";
+						}
+						break;
+					case "boolean":
+						properties += "\t\tpublic bool? "+columnName+" { get; set; }\n";
+						break;
+					case "number":
+						properties += "\t\tpublic decimal? "+columnName+" { get; set; }\n";
+						break;
+					default:
+				}
+			}
+		});
+
+
+
+
   $.ajax({
     url : "ExampleDTO.cs",
     dataType: "text",
     success : function (data) {
       var dto = data;
-      var entityName = $('#model-select').val();
       data = data.replace(/{{EntityName}}/g, entityName);
+	  data = data.replace(/{{Properties}}/g, properties);
+
       console.log(data);
+	  
   }});
   
 };
