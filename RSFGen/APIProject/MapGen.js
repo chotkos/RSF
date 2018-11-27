555ed214142a5e95a1db1f72c51ff8c1d187126c
Mateusz Chodkowski <chodkowskim@frontexlabs.local>
2018-11-27 11:10:39 +0100

    mapping fix

--- RSFGen/APIProject/MapGen.js
+++ RSFGen/APIProject/MapGen.js
@@ -3,6 +3,7 @@ var generateMap = function () {
 	var entityName = $('#model-select').val();
 	var properties = "\n";
 	var propertiesObj = document.data.definitions[entityName].properties;
+	var keyValueCounter = 0;
 
 	Object.keys(propertiesObj)
 		.forEach(columnName => {
@@ -12,27 +13,34 @@ var generateMap = function () {
 				switch (column.type) {
 					case "integer":
 						//provider.AddColumn("NumberInt", v.NumberInt, "");
-						properties += '\t\t\tprovider.AddColumn("' + columnName + '", v.' + columnName + ');\n';
+						properties += '\t\t\tprovider.AddColumn("' + columnName + '", v.' + columnName + '.ToString());\n';
 						break;
 					case "string":
-						properties += '\t\t\tprovider.AddColumn("' + columnName + '", v.' + columnName + ');\n';
+
+						if (column.format === "date-time") {
+							keyValueCounter++;
+							properties += '\t\t\tvar dateString' + keyValueCounter + ' = v.' + columnName + ' != null ? v.' + columnName + '.ToString("yyyy-MM-dd HH:mm:ss") : String.Empty ;\n';
+							properties += '\t\t\tprovider.AddColumn("' + columnName + '", dateString' + keyValueCounter + ');\n';
+						} else {
+							properties += '\t\t\tprovider.AddColumn("' + columnName + '", v.' + columnName + ');\n';
+						}
 						break;
 					case "boolean":
 						// if(v.Active.HasValue) provider.AddColumn("Active", (v.Active.Value ? 1:0).ToString(), string.Empty);
-						properties += '\t\t\if(v.' + columnName + '.HasValue) provider.AddColumn("' + columnName + '", (v.' + columnName + '.Value ? 1:0).ToString());';
+						properties += '\t\t\tif(v.' + columnName + '.HasValue) provider.AddColumn("' + columnName + '", (v.' + columnName + '.Value ? 1:0).ToString());\n';
 						break;
 					case "number":
 						properties += '\t\t\tprovider.AddColumn("' + columnName + '", v.' + columnName + ');\n';
 						break;
 					default:
-						if(column["$ref"])
-						{
+						if (column["$ref"]) {
+							keyValueCounter++;
 							var idSuffix = column["$ref"] ? 'Id' : '';
-							properties += '\t\t\tvar keyValue = v.'+columnName+idSuffix+'.HasValue ? v.'+columnName+idSuffix+'.Value.ToString() : String.Empty;\n';
-							properties += '\t\t\tprovider.AddColumn("' + columnName + idSuffix + '", keyValue);\n';
+							properties += '\t\t\tvar keyValue' + keyValueCounter + ' = v.' + columnName + idSuffix + '.HasValue ? v.' + columnName + idSuffix + '.Value.ToString() : String.Empty;\n';
+							properties += '\t\t\tprovider.AddColumn("' + columnName + idSuffix + '", keyValue' + keyValueCounter + ');\n';
 
-						} else{
-							properties += '\t\t\tprovider.AddColumn("' + columnName  + '", v.' + columnName  + ');\n';
+						} else {
+							properties += '\t\t\tprovider.AddColumn("' + columnName + '", v.' + columnName + ');\n';
 						}
 
 				}
